a8a70981a4e5ba5261b4a8e44120c2c738f0cb5a
hakimamarullah <hakimamarullah@gmail.com>
2025-07-13 21:40:38 +0700

    Fix SCC no setter for RedisCacheProp

--- src/main/java/com/starline/resi/config/RedisCacheProp.java
+++ src/main/java/com/starline/resi/config/RedisCacheProp.java
@@ -2,12 +2,14 @@ package com.starline.resi.config;
 
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @ConfigurationProperties(prefix = "redis.cache")
 @Component
 @Getter
+@Setter
 public class RedisCacheProp {
 
     private String host = "localhost";
