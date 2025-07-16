0fba9d0219f470ae261186ed575eace0559ba32d
hakimamarullah <hakimamarullah@gmail.com>
2025-07-16 22:54:06 +0700

    Add retry on scrapper feign

--- src/main/java/com/starline/resi/feign/ScrapperProxySvc.java
+++ src/main/java/com/starline/resi/feign/ScrapperProxySvc.java
@@ -6,6 +6,8 @@ import com.starline.resi.dto.proxy.ScrappingRequest;
 import com.starline.resi.feign.config.FeignBasicConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
         configuration = {FeignBasicConfig.class})
 public interface ScrapperProxySvc {
 
+    @Retryable(backoff = @Backoff(delay = 4000))
     @PostMapping(value = "/scrapper/tracking", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     ApiResponse<CekResiScrapResponse> scrap(@RequestBody ScrappingRequest payload);
 }
